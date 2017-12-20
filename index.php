043ce0052a0ea2bc6fe9042cf41dd7d2b8f6be62
smPark <smboy86@naver.com>
2017-12-20 14:35:23 +0900

    init get rest api

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-    phpinfo();
-?>
+require_once("/var/www/html/api/vendor/autoload.php");
+
+class MyTestIndex extends \CafeLatte\Core\BaseRoute implements \CafeLatte\Interfaces\RouterInterface
+{
+    public function routing()
+    {
+        //Write your php code here(GET/POST/DELETE/PUT)
+        echo "Hello CL PHP World";
+    }
+
+}
+
+$framework = new MyTestIndex("/var/www/html/api/cafelatte.json");
+$framework->execute();
